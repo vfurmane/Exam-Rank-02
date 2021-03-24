5e0d96151ecc86a763449868ce8dff09ba344670
vfurmane <vfurmane@student.42.fr>
2021-03-24 12:17:15 +0100

    Add get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,21 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/24 11:16:29 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/03/24 11:18:31 by vfurmane         ###   ########.fr       */
+/*   Created: 2021/03/24 11:15:48 by vfurmane          #+#    #+#             */
+/*   Updated: 2021/03/24 12:02:31 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#include "get_next_line.h"
 
-# include <unistd.h>
-# include <stdlib.h>
+int	get_next_line(char **line)
+{
+	int		i;
+	int		ret;
+	char	buffer;
 
-int	get_next_line(char **line);
-
-#endif
+	if (line == NULL)
+		return (-1);
+	*line = malloc(1234567);
+	if (*line == NULL)
+		return (-1);
+	i = 0;
+	while ((ret = read(0, &buffer, 1)) != 0 && buffer != '\n')
+	{
+		if (ret == -1)
+			return (-1);
+		(*line)[i++] = buffer;
+	}
+	(*line)[i++] = '\0';
+	if (buffer == '\n')
+		return (1);
+	return (0);
+}
